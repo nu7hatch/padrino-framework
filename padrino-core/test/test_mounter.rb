7ed40bd8692392c48ce04cc054a42d8ea2ddef28
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-16 21:59:34 -0700

    [padrino-core] test mounter convert

--- padrino-core/test/test_mounter.rb
+++ padrino-core/test/test_mounter.rb
@@ -1,99 +1,107 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper')
 
-class TestMounter < Test::Unit::TestCase
+context "Mounter" do
+  setup { Padrino.mounted_apps.clear }
 
-  def setup
-    Padrino.mounted_apps.clear
+  context "methods" do
+    setup { Padrino::Mounter.new("test", :app_file => "/path/to/test.rb").to("/test") }
+    asserts("new") { Padrino::Mounter }.responds_to :new
+    asserts_topic.kind_of Padrino::Mounter
+    asserts_topic.responds_to :to
+    asserts_topic.responds_to :map_onto
+    asserts(:name).equals "test"
+    asserts(:app_class).equals "Test"
+    asserts(:app_file).equals "/path/to/test.rb"
+    asserts(:uri_root).equals "/test"
+    asserts(:app_root).equals "/path/to"
   end
 
-  context 'for mounter functionality' do
+  context "locate_app_file with __FILE__" do
+    setup { Padrino::Mounter.new("test", :app_file => __FILE__).to("/test") }
+    asserts(:name).equals "test"
+    asserts(:app_class).equals "Test"
+    asserts(:app_file).equals __FILE__
+    asserts(:uri_root).equals "/test"
+    asserts(:app_root).equals File.dirname(__FILE__)
+  end
 
-    should 'check methods' do
-      mounter = Padrino::Mounter.new("test", :app_file => "/path/to/test.rb")
-      mounter.to("/test")
-      assert_kind_of Padrino::Mounter, mounter
-      assert_respond_to Padrino::Mounter, :new
-      assert_respond_to mounter, :to
-      assert_respond_to mounter, :map_onto
-      assert_equal "test", mounter.name
-      assert_equal "Test", mounter.app_class
-      assert_equal "/path/to/test.rb", mounter.app_file
-      assert_equal "/test", mounter.uri_root
-      assert_equal File.dirname(mounter.app_file), mounter.app_root
-    end
+  context "mounting" do
 
-    should 'check locate_app_file with __FILE__' do
-      mounter = Padrino::Mounter.new("test", :app_file => __FILE__)
-      mounter.to("/test")
-      assert_equal "test", mounter.name
-      assert_equal "Test", mounter.app_class
-      assert_equal __FILE__, mounter.app_file
-      assert_equal "/test", mounter.uri_root
-      assert_equal File.dirname(mounter.app_file), mounter.app_root
-    end
-
-    should 'mount an app' do
+    context "an app" do
       class ::AnApp < Padrino::Application; end
-      Padrino.mount_core("an_app")
-      assert_equal AnApp, Padrino.mounted_apps.first.app_obj
-      assert_equal ["core"], Padrino.mounted_apps.collect(&:name)
+      setup { Padrino.mount_core("an_app") }
+      asserts("app object") { Padrino.mounted_apps.first.app_obj }.equals AnApp
+      asserts("app names") { Padrino.mounted_apps.collect(&:name) }.equals ["core"]
     end
 
-    should 'mount a core' do
-      mounter = Padrino.mount_core("test", :app_file => __FILE__)
-      assert_equal "core", mounter.name
-      assert_equal "Test", mounter.app_class
-      assert_equal Test, mounter.app_obj
-      assert_equal __FILE__, mounter.app_file
-      assert_equal "/", mounter.uri_root
-      assert_equal File.dirname(mounter.app_file), mounter.app_root
+    context "a core" do
+      setup { Padrino.mount_core("test", :app_file => __FILE__) }
+      asserts(:name).equals 'core'
+      asserts(:app_class).equals 'Test'
+      asserts(:app_obj).equals Test
+      asserts(:app_file).equals __FILE__
+      asserts(:uri_root).equals "/"
+      asserts(:app_root).equals File.dirname(__FILE__)
     end
 
-    should 'mount a core to url' do
-      mounter = Padrino.mount_core("test", :app_file => __FILE__).to('/me')
-      assert_equal "core", mounter.name
-      assert_equal "Test", mounter.app_class
-      assert_equal Test, mounter.app_obj
-      assert_equal __FILE__, mounter.app_file
-      assert_equal "/me", mounter.uri_root
-      assert_equal File.dirname(mounter.app_file), mounter.app_root
+    context "a core to url" do
+      setup { Padrino.mount_core("test", :app_file => __FILE__).to('/me') }
+      asserts(:name).equals 'core'
+      asserts(:app_class).equals 'Test'
+      asserts(:app_file).equals __FILE__
+      asserts(:uri_root).equals '/me'
+      asserts(:app_root).equals File.dirname(__FILE__)
     end
 
-    should 'mount multiple apps' do
+    context "multiple apps" do
       class ::OneApp < Padrino::Application; end
       class ::TwoApp < Padrino::Application; end
+      setup do
+        Padrino.mount("one_app").to("/one_app")
+        Padrino.mount("two_app").to("/two_app")
+        # And testing no duplicates
+        Padrino.mount("one_app").to("/one_app")
+        Padrino.mount("two_app").to("/two_app")
+      end
 
-      Padrino.mount("one_app").to("/one_app")
-      Padrino.mount("two_app").to("/two_app")
-      # And testing no duplicates
-      Padrino.mount("one_app").to("/one_app")
-      Padrino.mount("two_app").to("/two_app")
-
-      assert_equal OneApp, Padrino.mounted_apps[0].app_obj
-      assert_equal TwoApp, Padrino.mounted_apps[1].app_obj
-      assert_equal 2, Padrino.mounted_apps.size, "should not mount duplicate apps"
-      assert_equal ["one_app", "two_app"], Padrino.mounted_apps.collect(&:name)
+      asserts("first app") { Padrino.mounted_apps[0].app_obj }.equals OneApp
+      asserts("second app") { Padrino.mounted_apps[1].app_obj }.equals TwoApp
+      asserts("no dups") { Padrino.mounted_apps }.size 2
+      asserts("app names") { Padrino.mounted_apps.collect(&:name) }.equivalent_to ["one_app", "two_app"]
     end
 
-    should 'change mounted_root' do
-      Padrino.mounted_root = "fixtures"
-      assert_equal Padrino.root("fixtures", "test", "app.rb"), Padrino.mounted_root("test", "app.rb")
-      Padrino.mounted_root = "apps"
-      assert_equal Padrino.root("apps", "test", "app.rb"), Padrino.mounted_root("test", "app.rb")
-      Padrino.mounted_root = nil
-      assert_equal Padrino.root("test", "app.rb"), Padrino.mounted_root("test", "app.rb")
-    end
+  end
 
-    should 'correctly instantiate a new padrino application' do
+  context "changing mounted_root" do
+    context "to fixtures" do
+      setup { Padrino.mounted_root = "fixtures" ; Padrino.mounted_root("test", "app.rb") }
+      asserts_topic.equals Padrino.root("fixtures", "test", "app.rb")
+    end
+    context "to apps" do
+      setup { Padrino.mounted_root = "apps" ; Padrino.mounted_root("test", "app.rb") }
+      asserts_topic.equals Padrino.root("apps", "test", "app.rb")
+    end
+    context "to nil" do
+      setup { Padrino.mounted_root = nil ; Padrino.mounted_root("test", "app.rb") }
+      asserts_topic.equals Padrino.root("test", "app.rb")
+    end
+  end
+
+  context "instantiate a new padrino application" do
+    setup do
       mock_app do
         get("/demo_1"){ "Im Demo 1" }
         get("/demo_2"){ "Im Demo 2" }
       end
-
-      get '/demo_1'
-      assert_equal "Im Demo 1", body
-      get '/demo_2'
-      assert_equal "Im Demo 2", body
+    end
+    context "/demo_1" do
+      setup { get '/demo_1' ; body }
+      asserts_topic.equals 'Im Demo 1'
+    end
+    context "/demo_2" do
+      setup { get '/demo_2' ; body }
+      asserts_topic.equals 'Im Demo 2'
     end
   end
+
 end
