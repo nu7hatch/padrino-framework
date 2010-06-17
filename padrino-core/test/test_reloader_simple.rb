d746c464b05954eea0bc34a64a4757dc8ba6c52c
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-17 00:20:47 -0700

    [padrino-core] convert test reloader simple

--- padrino-core/test/test_reloader_simple.rb
+++ padrino-core/test/test_reloader_simple.rb
@@ -1,96 +1,100 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper')
 require File.expand_path(File.dirname(__FILE__) + '/fixtures/apps/simple')
 
-class TestSimpleReloader < Test::Unit::TestCase
+context "Simple Reloader" do
 
-  context 'for simple reset functionality' do
-
-    should 'reset routes' do
+  context "reset" do
+    setup do
       mock_app do
-        (1..10).each do |i|
-          get("/#{i}") { "Foo #{i}" }
-        end
-      end
-      (1..10).each do |i|
-        get "/#{i}"
-        assert_equal "Foo #{i}", body
-      end
-      @app.reset_routes!
-      (1..10).each do |i|
-        get "/#{i}"
-        assert_equal 404, status
+        (1..10).each { |i| get("/#{i}") { "Foo #{i}" } }
       end
     end
+    (1..10).each { |i| asserts("/#{i}") { get "/#{i}" ; body }.equals "Foo #{i}" }
 
-    should 'keep sinatra routes on development' do
+    context "after reset" do
+      setup { @app.reset_routes! }
+      (1..10).each { |i| asserts("/#{i}") { get "/#{i}" ; status }.equals 404 }
+    end
+
+  end
+
+  context "keep sinatra routes on development" do
+    setup do
       mock_app do
         set :environment, :development
-        get("/"){ "ok" }
+        get("/") { 'ok' }
       end
-      assert_equal :development, @app.environment
-      get "/"
-      assert_equal 200, status
-      get "/__sinatra__/404.png"
-      assert_equal 200, status
-      assert_equal "image/png", response["Content-Type"]
-      @app.reset_routes!
-      get "/"
-      assert_equal 404, status
-      get "/__sinatra__/404.png"
-      assert_equal 200, status
-      assert_equal "image/png", response["Content-Type"]
     end
+    asserts(:environment).equals :development
+    asserts("/") { get "/" ; status }.equals 200
+    asserts("sinatra 404") {  get "/__sinatra__/404.png" ; status }.equals 200
+    asserts("content type") { get "/__sinatra__/404.png" ; response["Content-Type"] }.equals 'image/png'
+
+    context "reset!" do
+      setup { @app.reset_routes! }
+      asserts("/") { get "/" ; status }.equals 404
+      asserts("sinatra 404") {  get "/__sinatra__/404.png" ; status }.equals 200
+      asserts("content type") { get "/__sinatra__/404.png" ; response["Content-Type"] }.equals 'image/png'
+    end
+
   end
 
-  context 'for simple reload functionality' do
+  context "reload" do
 
-    should 'correctly instantiate SimpleDemo fixture' do
-      Padrino.mounted_apps.clear
-      Padrino.mount_core("simple_demo")
-      assert_equal ["core"], Padrino.mounted_apps.collect(&:name)
-      assert SimpleDemo.reload?
-      assert_match %r{fixtures/apps/simple.rb}, SimpleDemo.app_file
+    context "instantiate SimpleDemo fixture" do
+      setup { Padrino.mounted_apps.clear }
+      setup { Padrino.mount_core("simple_demo") }
+      asserts("app names") { Padrino.mounted_apps.collect(&:name) }.equals ['core']
+      asserts("reloads") { SimpleDemo.reload? }
+      asserts("file") { SimpleDemo.app_file }.matches %r{fixtures/apps/simple.rb}
     end
 
-    should 'correctly reload SimpleDemo fixture' do
-      @app = SimpleDemo
-      get "/"
-      assert ok?
-      new_phrase = "The magick number is: #{rand(100)}!"
-      buffer     = File.read(SimpleDemo.app_file)
-      new_buffer = buffer.gsub(/The magick number is: \d+!/, new_phrase)
-      File.open(SimpleDemo.app_file, "w") { |f| f.write(new_buffer) }
-      sleep 1.2 # We need at least a cooldown of 1 sec.
-      get "/"
-      assert_equal new_phrase, body
+    context "reload SimpleDemo fixture" do
+      setup { @app = SimpleDemo }
+      asserts("/") { get '/' ; ok? }
 
-      # Now we need to prevent to commit a new changed file so we revert it
-      File.open(SimpleDemo.app_file, "w") { |f| f.write(buffer) }
-      Padrino.reload!
-    end
+      context "rewrite" do
+        setup do
+          @buffer = File.read(SimpleDemo.app_file)
+          new_buffer = @buffer.gsub(/The magick number is: \d+!/, "The magick number is: 12345!")
+          File.open(SimpleDemo.app_file, "w") { |f| f.write(new_buffer) }
+          sleep 1.2 # We need at least a cooldown of 1 sec.
+        end
+        teardown do
+          # Now we need to prevent to commit a new changed file so we revert it
+          File.open(SimpleDemo.app_file, "w") { |f| f.write(@buffer) }
+          Padrino.reload!
+        end        
+        asserts("returns right body") { get '/'; body }.equals "The magick number is: 12345!"
+      end
 
-    should 'correctly reset SimpleDemo fixture' do
-      @app = SimpleDemo
-      get "/rand"
-      assert ok?
-      last_body = body
-      assert_equal 2, @app.before_filters.size # one is ours the other is default_filter for content type
-      assert_equal 1, @app.errors.size
-      assert_equal 1, @app.after_filters.size
-      assert_equal 2, @app.middleware.size # [Padrino::Logger::Rack, Padrino::Reloader::Rack]
-      assert_equal 4, @app.routes.size # GET+HEAD of "/" + GET+HEAD of "/rand" = 4
-      assert_equal 2, @app.extensions.size # [Padrino::Routing, Padrino::Rendering]
-      assert_equal 0, @app.templates.size
-      @app.reload!
-      get "/rand"
-      assert_not_equal last_body, body
-      assert_equal 2, @app.before_filters.size # one is ours the other is default_filter for content type
-      assert_equal 1, @app.errors.size
-      assert_equal 1, @app.after_filters.size
-      assert_equal 2, @app.middleware.size # only logger Padrino::Logger::Rack
-      assert_equal 4, @app.routes.size # GET+HEAD of "/" = 2
-      assert_equal 2, @app.extensions.size # [Padrino::Routing, Padrino::Rendering]
-      assert_equal 0, @app.templates.size
     end
+  
+    context "reset SimpleDemo fixture" do
+      setup { @app = SimpleDemo }
+      asserts("/rand") { get '/rand' ; @last = body; ok? }
+      asserts(:before_filters).size 2 # one is ours the other is default_filter for content type
+      asserts(:errors).size 1
+      asserts(:after_filters).size 1
+      asserts(:middleware).size 2 # [Padrino::Logger::Rack, Padrino::Reloader::Rack]
+      asserts(:routes).size 4 # GET+HEAD of "/" + GET+HEAD of "/rand" = 4
+      asserts(:extensions).size 2 # [Padrino::Routing, Padrino::Rendering]
+      asserts(:templates).size 0
+      
+      context "reload!" do
+        setup { @app.reload! ; @app }
+        asserts("/rand") { get '/rand' ; @last != body }
+        asserts(:before_filters).size 2 # one is ours the other is default_filter for content type
+        asserts(:errors).size 1
+        asserts(:after_filters).size 1
+        asserts(:middleware).size 2 # [Padrino::Logger::Rack, Padrino::Reloader::Rack]
+        asserts(:routes).size 4 # GET+HEAD of "/" + GET+HEAD of "/rand" = 4
+        asserts(:extensions).size 2 # [Padrino::Routing, Padrino::Rendering]
+        asserts(:templates).size 0
+      end
+      
+    end
+  
   end
+
 end
