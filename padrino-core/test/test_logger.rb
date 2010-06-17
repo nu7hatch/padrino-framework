6e922e179bebd07ecba3db5c596bf95e50a8b705
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-16 21:35:09 -0700

    [padrino-core] convert logger

--- padrino-core/test/test_logger.rb
+++ padrino-core/test/test_logger.rb
@@ -1,55 +1,52 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper')
 
-class TestPadrinoLogger < Test::Unit::TestCase
-
-  def setup
+context "Padrino Logger" do
+  setup do
     Padrino::Logger::Config[:test][:stream] = :null # The default
     Padrino::Logger.setup!
   end
 
-  def setup_logger(options={})
-    @log    = StringIO.new
-    @logger = Padrino::Logger.new(options.merge(:stream => @log))
-  end
+  context 'stream configuration' do
+    context "default" do
+      setup { Padrino.logger.log }
+      asserts_topic.kind_of StringIO
+    end
 
-  context 'for logger functionality' do
-
-    context 'check stream config' do
-
-      should 'use stdout if stream is nil' do
+    context 'if stream is nil' do
+      setup do
         Padrino::Logger::Config[:test][:stream] = nil
         Padrino::Logger.setup!
-        assert_equal $stdout, Padrino.logger.log
+        Padrino.logger.log
       end
-
-      should 'use StringIO as default for test' do
-        assert_instance_of StringIO, Padrino.logger.log
-      end
-
-      should 'use a custom stream' do
-        my_stream = StringIO.new
-        Padrino::Logger::Config[:test][:stream] = my_stream
-        Padrino::Logger.setup!
-        assert_equal my_stream, Padrino.logger.log
-      end
+      asserts_topic.equals $stdout
     end
 
-    should 'log something' do
-      setup_logger(:log_level => :error)
-      @logger.error "You log this error?"
-      assert_match(/You log this error?/, @log.string)
-      @logger.debug "You don't log this error!"
-      assert_no_match(/You don't log this error!/, @log.string)
-      @logger << "Yep this can be logged"
-      assert_match(/Yep this can be logged/, @log.string)
+    asserts 'using custom stream' do
+      my_stream = StringIO.new
+      Padrino::Logger::Config[:test][:stream] = my_stream
+      Padrino::Logger.setup!
+      my_stream == Padrino.logger.log
+    end
+  end
+
+  context "logging" do
+    context "something" do
+      setup do
+        @log = StringIO.new
+        Padrino::Logger.new(:log_level => :error, :stream => @log)
+      end
+      asserts("logging debug") { topic.debug "You dont log!" ; @log.string =~ %r{You dont log!}}.not!
+      asserts("logging error") { topic.error "log error!"; @log.string }.matches  %r{log error!}
+      asserts("logging it") { topic<<("This too") ; @log.string }.matches %r{This too}
     end
 
-    should 'log an application' do
-      mock_app { get("/"){ "Foo" } }
-      get "/"
-      assert_equal "Foo", body
-      assert_match /GET \/  - 200/, Padrino.logger.log.string
+    context 'application' do
+      setup do
+        mock_app { get("/"){ "Foo" } }
+        get "/"
+      end
+      asserts("foo") { body }.equals "Foo"
+      asserts("GET 200") { Padrino.logger.log.string }.matches %r{GET \/  - 200}
     end
-
   end
 end
